2956a5af03774b3b65022a4821a1ceb16f9da87b
qingyujean <995168634@qq.com>
2021-05-26 14:00:53 +0800

    ch4_8 实现一个copy assignment operator

--- essential_c_plus_plus/ch4/ch4_8/Matrix.cpp
+++ essential_c_plus_plus/ch4/ch4_8/Matrix.cpp
@@ -12,5 +12,19 @@ Matrix::Matrix(const Matrix &rhs): _row(rhs._row), _col(rhs._col){ // copy构造
         _pmat[ix] = rhs._pmat[ix];
 }
 
+Matrix & Matrix::operator=(const Matrix &rhs){
+    if(this != &rhs){
+        _row = rhs._row;
+        _col = rhs._col;
+        int elem_cnt = _row*_col;
+        delete [] _pmat;
+        _pmat = new double[elem_cnt];
+        for(int ix=0; ix<elem_cnt; ++ix)
+            _pmat[ix] = rhs._pmat[ix];
+    }
+    return *this;
+}
+
 int main(){
+    
 }
---
+++ essential_c_plus_plus/ch4/ch4_8/Matrix.h
@@ -0,0 +1,18 @@
+
+using namespace std;
+
+class Matrix{
+public:
+    Matrix(int row, int col): _row(row), _col(col){
+        _pmat = new double[row*col];
+    }
+
+    ~Matrix(){
+        delete []_pmat;
+    }
+
+    Matrix & operator=(const Matrix &rhs);
+private:
+    int _row, _col;
+    double *_pmat;
+};

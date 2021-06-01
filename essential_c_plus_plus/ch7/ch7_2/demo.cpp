a1fe557dcd713a9fc2f8ebfd4641de57d549901d
qingyujean <995168634@qq.com>
2021-06-01 14:26:53 +0800

    ch7_2 捕获异常

---
+++ essential_c_plus_plus/ch7/ch7_2/demo.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <iostream>
+
+#include "iterator_overflow.h"
+
+using namespace std;
+
+// 声明
+extern void log_message(const char *);
+extern string err_message[];
+extern ostream log_file;
+
+bool some_function(){
+    bool status = true;
+
+    catch(int errno){
+        log_message( err_message[errno] );
+        status = false;
+    }
+    catch(const char *str){
+        log_message( str );
+        status = false;
+    }
+    catch(iterator_overflow &iof){
+        iof.what_happened(log_file);
+        status = false;
+    }
+    catch( ... ){ // 捕获任何异常
+        log_message( "exception of unknown type" ); 
+        status = false;
+    }
+
+    return status;
+}
+
+int main(){
+
+}
